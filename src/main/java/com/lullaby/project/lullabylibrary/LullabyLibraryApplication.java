05bff317d24a8551a76fb3ac9c2471d212481b13
lullaby <sds901234@naver.com>
2022-12-31 14:09:06 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/lullaby/project/lullabylibrary/LullabyLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.lullaby.project.lullabylibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LullabyLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LullabyLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lullaby/project/lullabylibrary/LullabyLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lullaby.project.lullabylibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LullabyLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
